53299c98399ea30217743d37cdb878da42d05a49
Tomm Smith <root.packet@gmail.com>
2023-01-02 12:54:15 -0500

    No. 399 - Introduced v0.1 Beta of main.py for Bonus Challenges.

--- Chal. No. 399 - Letter Value Sum/main.py
+++ Chal. No. 399 - Letter Value Sum/main.py
@@ -13,7 +13,6 @@ lettersum("microspectrophotometries") => 317
 """
 
 
-import os
 
 
 wrkng_dir = "C:\\Users\\rootp\\Documents\\Code\\Python\\DailyProgrammer\\Chal. No. 399 - Letter Value Sum"
@@ -21,10 +20,8 @@ data_file = "enable1.txt"
 
 
 alph = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, \
-        'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, \
-        'w': 23, 'x': 24, 'y': 25, 'z': 26}
-
-input_data = ["", "a", "z", "cab", "excellent", "microspectrophotometries"]
+        'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, \
+        't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
 
 
 
@@ -32,7 +29,7 @@ input_data = ["", "a", "z", "cab", "excellent", "microspectrophotometries"]
 """
     Global function definitions
 """
-def sum_of_chars(char_string):
+def lettersum(char_string):
     """ Loop over the chars of char_string, sum up the numeric representation, and return.
     
             Returns:
@@ -46,7 +43,7 @@ def sum_of_chars(char_string):
         str(char_string)
         
     except:
-        print("TypeError: sum_of_chars expected string, got other.")
+        print("TypeError: lettersum() expects string, got other.")
         return 1
 
     # Loop the provided string and add sums
@@ -61,8 +58,6 @@ def sum_of_chars(char_string):
     return char_sum
     
 
-
-
 """
     void main();
 """
@@ -75,41 +70,86 @@ except FileNotFoundError:
     
     
     
-# Loop input_data and print out string sums
-i = 0
+# Loop input_data and populate information
+""" word_struct = {} - Data Structure
+    
+    word_struct = {"word_sum" : [qty, ["word1", "word2", "word3", "etc"],
+                   "5"        : [2, ["hello", "world"]],
+                   "7"        : [3, ["empathy", "matress", "earlier"]],
+                   "2"        : [6, ["to", "of", "we", "no", "be", "at"]]
+                  } """
+word_struct = {}
+odd_lttrs = 0
+
+
 for word in fd:
+    word_sum = lettersum(word)
+    
     """ 1). microspectrophotometries is the only word with a letter sum of 317. 
             Find the only word with a letter sum of 319."""
-    if len(word) == 319:
+    if word_sum == 319:
+        print("1) Find the only word with a letter sum of 319: ", end="")
         print(word + " => " + str(len(word)))
 
     
-    """ 2). How many words have an odd letter sum?"""
+    """ 2). How many words have an odd letter sum? - preprocessing"""
+    if (word_sum % 2) == 1:
+        odd_lttrs += 1
+    
+    
+    # Populate our data structure for post processing
+    if not word_struct.get(word_sum):
+        word_struct[str(word_sum)] = [0, []]
+        
+    word_struct[str(word_sum)][0] += 1
+    word_struct[str(word_sum)][1].append(word)
+        
     
     
     
-    """ 3). There are 1921 words with a letter sum of 100, making it the second 
-            most common letter sum. What letter sum is most common, and how many 
-            words have it?"""
+""" 2). How many words have an odd letter sum?"""
+print("2) How many words have an odd letter Sum: ", end="")
+print(odd_lttrs)
     
     
+""" 3). There are 1921 words with a letter sum of 100, making it the second 
+        most common letter sum. What letter sum is most common, and how many 
+        words have it?"""
+ceil = 0
+ceil_id = 0
+
+# Loop data structure and seek most common letter sum
+for key in word_struct.keys():
+    if data_struct[key][0] > ceil:
+        ceil = data_struct[key][0]
+        ceil_id = key
+                
+print("2) What letter sum is most common, and how many words have it: ")
+print("    Most common: ", end="")
+print(ceil_id)
+print("    Word Qty: ", end="")
+print(ceil)
+
+        
+""" 4). zyzzyva and biodegradabilities have the same letter sum as each other 
+        (151), and their lengths differ by 11 letters. Find the other pair of 
+        words with the same letter sum whose lengths differ by 11 letters."""
+for x_word in data_structp['151'][1]:
+    for word in data_struct['151'][1]:
+        if len(word) > 11:
             
-    """ 4). zyzzyva and biodegradabilities have the same letter sum as each other 
-            (151), and their lengths differ by 11 letters. Find the other pair of 
-            words with the same letter sum whose lengths differ by 11 letters."""
-
-            
-          
-    """ 5). cytotoxicity and unreservedness have the same letter sum as each other 
-            (188), and they have no letters in common. Find a pair of words that 
-            have no letters in common, and that have the same letter sum, which is 
-            larger than 188. (There are two such pairs, and one word appears in 
-            both pairs.)"""
+        
+      
+""" 5). cytotoxicity and unreservedness have the same letter sum as each other 
+        (188), and they have no letters in common. Find a pair of words that 
+        have no letters in common, and that have the same letter sum, which is 
+        larger than 188. (There are two such pairs, and one word appears in 
+        both pairs.)"""
 
 
-  
-    """ 6). The list of word { geographically, eavesdropper, woodworker, oxymorons } 
-    contains 4 words. Each word in the list has both a different number of letters, 
-    and a different letter sum. The list is sorted both in descending order of word 
-    length, and ascending order of letter sum. What's the longest such list you can 
-    find?"""
+
+""" 6). The list of word { geographically, eavesdropper, woodworker, oxymorons } 
+contains 4 words. Each word in the list has both a different number of letters, 
+and a different letter sum. The list is sorted both in descending order of word 
+length, and ascending order of letter sum. What's the longest such list you can 
+find?"""
